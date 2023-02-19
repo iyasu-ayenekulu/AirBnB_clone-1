9fa9da939e2e70c76507706eebd34e9219849408
josh <iyasuayenekulu3@gmail.com>
2023-02-19 13:05:05 -0800

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
 
+"""
+A module that defines a User class which inherits BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Defines all common attribute/methods for other classes
+    Attr:
+        state_id (str)
+        name (str)
+    """
+    name = ""
+    state_id = ""
